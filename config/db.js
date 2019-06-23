fc729a94e391e8a85556541e910c1eede3c3835f
Kostiv Bogdan <boh.kokos@gmail.com>
2019-06-23 17:37:49 +0300

    cleaning

--- config/db.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    url: "mongodb+srv://sinchez:chester123@cluster0-nw60h.mongodb.net/test?retryWrites=true&w=majority",
-    dbName: 'todoProject',
-    options: { useNewUrlParser: true }
-};
--- routes/routes.js
+++
@@ -1,6 +0,0 @@
-const mainPage = require('../app/modules/main-page/main-page.js');
-
-
-module.exports = (app, db) => {
-    mainPage(app, db);
-}
