2440b3590a1afa3358945977a9dd625daaabefea
ViktorFlakey <viktor.flakey@gmail.com>
2019-07-12 20:09:45 +0300

    Updates notes

--- routes/read/read-note.js
+++ routes/read/read-note.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../../models/User");
+const Notes = require("../../models/notes");
+const { ensureAuthenticated } = require("../../config/identify-check.js");
 
-
+router.get("/:id" ,ensureAuthenticated, (req,res) => {
+    const ListID = req.params.id;
+    console.log(ListID);
+    Notes.find({_id: ListID})
+        .then(data => {
+					const noteTitle = data[0]["noteTitle"];
+					const noteDescript = data[0]["noteDescript"];
+					const ID = data[0]["_id"];
+					console.log(ListID);
+            res.render("detailsNotes", {noteTitle,noteDescript,ID});
+        })
+        .catch(err => console.log(err))
+});
 
 
 module.exports = router;
--- routes/update/update-file.js
+++ routes/update/update-file.js
@@ -1,35 +1,47 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../../models/User");
+const Notes = require("../../models/notes");
+const {ensureAuthenticated  } = require("../../config/identify-check.js");
 
-router.put('notes/:id' , (req,res)=>{
-	 User.update(
-		 {_id: req.params.id},
-		 {	notetitle: req.body.noteTitle,
-				notedescrip: req.body.noteDescript
-		 },(err,result) =>{
-			 if(err) {
-				 console.log(err);
-				 return res.sendStatus(500);
-			 }
-			 res.redirect('/dashboard')
-		 }
-	 )
-});
-router.put('list/:id' , (req,res)=>{
-	User.update(
-		{_id: req.params.id},
-		{	notetitle: req.body.noteTitle,
-			   notedescrip: req.body.noteDescript
-		},(err,result) =>{
-			if(err) {
-				console.log(err);
-				return res.sendStatus(500);
-			}
-			res.redirect('/dashboard')
-		}
-	)
+router.post('/update', ensureAuthenticated, async (req,res)=>{
+ let ID = req.body.ID;
+ console.log(req.body);
+	await Notes.findByIdAndUpdate(ID, {	noteTitle: req.body.noteTitle, noteDescript: req.body.noteDescript }, (err, note) => {
+		console.log(note);
+			if(err) throw err;
+			
+	})
+	res.redirect('/dashboard');
+	// res.render('updateNote', {note: newNote})
 })
+// router.put('notes/:id' , (req,res)=>{
+// 	 User.update(
+// 		 {_id: req.params.id},
+// 		 {	notetitle: req.body.noteTitle,
+// 				notedescrip: req.body.noteDescript
+// 		 },(err,result) =>{
+// 			 if(err) {
+// 				 console.log(err);
+// 				 return res.sendStatus(500);
+// 			 }
+// 			 res.redirect('/dashboard')
+// 		 }
+// 	 )
+// });
+// router.put('list/:id' , (req,res)=>{
+// 	User.update(
+// 		{_id: req.params.id},
+// 		{	notetitle: req.body.noteTitle,
+// 			   notedescrip: req.body.noteDescript
+// 		},(err,result) =>{
+// 			if(err) {
+// 				console.log(err);
+// 				return res.sendStatus(500);
+// 			}
+// 			res.redirect('/dashboard')
+// 		}
+// 	)
+// })
 
 
 
